5a588237c0fe1c81354a9202e29a9ef5b69591b0
Panupong Rong-O <tayalone@gmail.com>
2022-08-24 13:10:00 +0700

    ✨ สร้าง simple repository

--- main.go
+++ main.go
@@ -1,14 +1,73 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type todo struct {
+	UserID    uint   `json:"userId"`
+	ID        uint   `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func (t *todo) set(title string, completed bool) {
+	t.Title = title
+	t.Completed = completed
+}
+
+type todos []todo
+
+func (ts todos) findAll() []todo {
+	return ts
+}
+
+func (ts todos) findByID(i int) (todo, error) {
+	if len(ts)-1 < i {
+		return todo{}, errors.New("id doest does not exist")
+	}
+	return ts[i], nil
+}
+
+func (ts *todos) setByID(i int, title string, completed bool) (todo, error) {
+	if len(*ts)-1 < i {
+		return todo{}, errors.New("id doest does not exist")
+	}
+	(*ts)[i].set(title, completed)
+
+	return (*ts)[i], nil
+}
+
+func (ts *todos) removeBy(i int) error {
+	if len(*ts)-1 < i {
+		return errors.New("id doest does not exist")
+	}
+	s := *ts
+	s = append(s[:i], s[i+1:]...)
+	*ts = s
+	return nil
+}
+
+var todoRepo todos = todos{
+	todo{
+		UserID:    1,
+		ID:        1,
+		Title:     "Lorem 1",
+		Completed: true,
+	},
+	todo{
+		UserID:    1,
+		ID:        2,
+		Title:     "Lorem 1",
+		Completed: true,
+	},
+}
+
 func main() {
-	fmt.Println("x")
 	r := gin.Default()
 	r.GET("/status", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
